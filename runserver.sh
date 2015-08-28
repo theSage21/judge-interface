43837eeadea08f7b26003b50228a1231167e10c5
theSage21 <arjoonn.94@gmail.com>
2015-08-28 08:52:09 +0530

    Added LOCATION to rnserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 source env/bin/activate
-
-cd openJudge/webserver
-# gunicorn website.wsgi:application --bind=127.0.0.1:8000 --log-file=-
-gunicorn website.wsgi:application --bind=unix:/home/ghost/dev/openJudge/openJudge/gunicorn_socket --log-file=-
+cd webserver
+gunicorn website.wsgi:application --bind=127.0.0.1:8000 --log-file=-
+# gunicorn website.wsgi:application --bind=unix:LOCATION/gunicorn_socket--log-file=-
