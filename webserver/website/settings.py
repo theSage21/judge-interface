f4287dcab5f5ee8faa2ef4b8e031d85a82355538
theSage21 <arjoonn.94@gmail.com>
2015-08-28 09:22:19 +0530

    Staticfile changes

--- webserver/website/settings.py
+++ webserver/website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     'bootstrapform',
     'question',
     'contest',
