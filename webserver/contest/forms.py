d1fd69085e515ae3dafc290e5d2358818cd04558
theSage21 <arjoonn.94@gmail.com>
2015-08-30 21:35:07 +0530

    - Registration prevents duplicate registration

--- webserver/contest/forms.py
+++ webserver/contest/forms.py
@@ -1,6 +1,18 @@
 from django.forms import Form, CharField, PasswordInput
+from django.contrib.auth.models import User
 
 
 class RegistrationForm(Form):
     username = CharField(label='Username: ', max_length=50)
     password = CharField(widget=PasswordInput)
+    def is_valid(self):
+        valid = super (Form, self).is_valid()
+        if not valid:
+            return valid
+        uname = self.cleaned_data['username']
+        users = User.objects.filter(username=uname).count()
+        if users > 0:
+            self._errors['Validation_Error'] = 'The username is taken. Pick a new one'
+            return False
+        else:
+            return True
