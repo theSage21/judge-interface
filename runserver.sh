6a7b59f05c3352d56d5a4f432e016e1ca39f5ab7
Arjoonn Sharma <arjoonn.94@gmail.com>
2015-11-07 17:25:26 +0530

    - Collect static to runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 source env/bin/activate
 cd webserver
+python manage.py collectstatic --noinput
 gunicorn website.wsgi:application --bind=127.0.0.1:8000 --log-file=-
 # gunicorn website.wsgi:application --bind=unix:/home/ghost/dev/InterfaceJudge/gunicorn_socket --log-file=-
