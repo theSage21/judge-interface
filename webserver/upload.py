d7dafb0b9b77f62d4d2a1695fbcbdaf12b70f479
Arjoonn Sharma <arjoonn.94@gmail.com>
2015-11-08 21:39:08 +0530

    - Ace editor font size - Robust upload.py

--- webserver/upload.py
+++ webserver/upload.py
@@ -6,6 +6,7 @@ django.setup()
 from django.core.files import File
 from question.models import Answer, Question, AnswerType
 
+
 def read(qno, filename):
     path = os.path.join(str(qno), filename)
     with open(path, 'r') as fl:
@@ -13,19 +14,28 @@ def read(qno, filename):
     return data
 
 print('Adding questions')
-questions = '1234567'
+questions = ''.join(map(str, range(1, 100)))
 for ques in questions:
     q = Question()
     q.qno = int(ques)
-    q.title = read(ques, 'title')
-    q.text = read(ques, 'text')
-    if (q == '1') or (q == '2'):
-        q.practice = True
-    q.save()
-    ans = Answer()
-    ans.question = q
-    ans.infile = File(open(ques + '/inp', 'r'))
-    ans.outfile = File(open(ques + '/out', 'r'))
-    ans.sample_code = File(open(ques + '/sample', 'r'))
-    ans.answer_type = AnswerType.objects.first()
-    ans.save()
+    try:
+        q.title = read(ques, 'title')
+        q.text = read(ques, 'text')
+        if (q == '1') or (q == '2'):
+            q.practice = True
+        q.save()
+    except:
+        print('Could not add Q-', ques)
+    else:
+        try:
+            ans = Answer()
+            ans.question = q
+            ans.infile = File(open(ques + '/inp', 'r'))
+            ans.outfile = File(open(ques + '/out', 'r'))
+            ans.sample_code = File(open(ques + '/sample', 'r'))
+            ans.answer_type = AnswerType.objects.first()
+            ans.save()
+        except:  # remove the question too
+            print('Could not add A-', ques)
+            q.delete()
+            print('Deleted Q-', ques)
